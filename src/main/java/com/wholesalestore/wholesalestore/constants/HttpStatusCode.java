d22acd25243beea31e17d841ffd5c1a70c78005f
Arvind Kumar <arvind.iitrmca@gmail.com>
2020-04-22 01:06:13 +0530

    Added constants package, HttpStatusCode and RegistrationConstant class.

---
+++ src/main/java/com/wholesalestore/wholesalestore/constants/HttpStatusCode.java
@@ -0,0 +1,8 @@
+package com.wholesalestore.wholesalestore.constants;
+
+public interface HttpStatusCode {
+    int SUCCESS = 200;
+    int INTERNAL_SERVER_ERROR = 500;
+    String RESPONSE = "response";
+    String STATUS = "status";
+}
---
+++ src/main/java/com/wholesalestore/wholesalestore/constants/RegistrationConstant.java
@@ -0,0 +1,6 @@
+package com.wholesalestore.wholesalestore.constants;
+
+public interface RegistrationConstant {
+    String EMAIL_EXIST = "Email id already in use.";
+    String REGISTERED = "Registered successfully.";
+}
