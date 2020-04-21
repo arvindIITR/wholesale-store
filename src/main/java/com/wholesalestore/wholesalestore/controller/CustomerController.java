9e3bd2c7600bfe8cc2ee12964082957b2ed32c72
Arvind Kumar <arvind.iitrmca@gmail.com>
2020-04-22 00:58:14 +0530

    Added http response code and changed register API return type to Map<String, Object> from ResponseEntity<?>.

--- src/main/java/com/wholesalestore/wholesalestore/controller/CustomerController.java
+++ src/main/java/com/wholesalestore/wholesalestore/controller/CustomerController.java
@@ -1,16 +1,18 @@
 package com.wholesalestore.wholesalestore.controller;
 
+import com.wholesalestore.wholesalestore.constants.HttpStatusCode;
 import com.wholesalestore.wholesalestore.customers.CustomerService;
 import com.wholesalestore.wholesalestore.entities.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * @Author arvindkumar
+ * @author arvindkumar
  * created on 21/04/20
  */
 
@@ -22,19 +24,30 @@ public class CustomerController {
 
 
     @PostMapping(value = "/register")
-    public ResponseEntity resisterCustomer(@RequestBody Customer customer){
+    public Map<String, Object> resisterCustomer(@RequestBody Customer customer){
+        Map<String, Object> response = new HashMap<>();
         String email = customer.getEmail();
         if(email != null){
             Customer customerFromDb = customerService.getCustomerByEmail(email);
             if(customerFromDb != null){
-                return ResponseEntity.ok().body("Email id already in use");
+                response.put("response", "Email id already in use");
+                response.put("status", 201);
+                return response;
             }
         }
+        Customer persistedCustomer;
         try{
-            customerService.createCustomer(customer);
+            persistedCustomer = customerService.createCustomer(customer);
         }catch (Exception e){
             e.printStackTrace();
+            response.put("response", "Internal server error");
+            response.put("status", HttpStatusCode.INTERNAL_SERVER_ERROR);
+            return response;
         }
-        return ResponseEntity.ok().body("200");
+        if(persistedCustomer != null){
+            response.put("response", "Successfully registered.");
+            response.put("status", HttpStatusCode.SUCCESS);
+        }
+        return response;
     }
 }
