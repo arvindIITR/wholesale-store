5c8e60864863fa838b074803242b9c48e29d652c
Arvind Kumar <arvind.iitrmca@gmail.com>
2020-04-26 01:15:16 +0530

    Added spring session schema script file schema.sql and test case for DB data source connection.

--- src/main/java/com/wholesalestore/wholesalestore/WholesaleStoreApplication.java
+++ src/main/java/com/wholesalestore/wholesalestore/WholesaleStoreApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 @EntityScan(basePackages = {"com.wholesalestore.wholesalestore.entities"})
+@EnableJdbcHttpSession
 public class WholesaleStoreApplication {
 
 	public static void main(String[] args) {
--- src/test/java/com/wholesalestore/wholesalestore/WholesaleStoreApplicationTests.java
+++ src/test/java/com/wholesalestore/wholesalestore/WholesaleStoreApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wholesalestore.wholesalestore;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class WholesaleStoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	DataSource dataSource;
 
+	@Test
+	public void testschema() throws SQLException {
+		String schema = dataSource.getConnection().getCatalog();
+		assertTrue("wholesalestore1".equalsIgnoreCase(schema));
+	}
 }
